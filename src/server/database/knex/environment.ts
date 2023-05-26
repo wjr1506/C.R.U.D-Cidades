2f81e254ec6b761d6e09f2920e5fa3742ed2119d
Wellinton Neves Lucena Junior <wellintonneves.51@gmail.com>
2023-05-26 04:19:35 -0300

    config to postgress

--- src/server/database/knex/environment.ts
+++ src/server/database/knex/environment.ts
@@ -1,5 +1,6 @@
 import { Knex } from 'knex';
 import path from 'path';
+import { number } from 'yup';
 
 export const development: Knex.Config = {
   client: 'sqlite3',
@@ -25,5 +26,19 @@ export const test: Knex.Config = {
   connection: ':memory:'
 }
 export const prod: Knex.Config = {
-  ...development
+  client: 'pg',
+  connection: {
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    database:process.env.DATABASE_NAME,
+    password:process.env.DATABASE_PASSWORD,
+    port:Number(process.env.DATABASE_PORT )|| 5423,
+    ssl:{rejectUnauthorized: false},
+  },
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds')
+  },
 }
